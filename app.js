69ea318961e2588ed58f4675d62b65de91739b4c
mustafa-mujtaba <urdustory0623@gmail.com>
2024-05-26 04:46:48 +0500

    adding my word counter project

---
+++ app.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Display a Colourfull Welcome Message
+console.log(chalk.bold.cyanBright("\n \t Muhammad Mehdi Raza"));
+console.log("=".repeat(60));
+// Prompt the User to Enter a Sentance
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "enter a sentence",
+    }
+]);
+// Trimming the sentence and splitting it in to words based on "space"
+let words = answers.sentence.trim().split(" ");
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
